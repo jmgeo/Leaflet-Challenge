3fdddbd51a3fc4594c00e262b996f0eb06832e20
jmgeo <j.mills.geo@gmail.com>
2021-08-09 14:29:09 -0500

    Working through it.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,8 +1,9 @@
 //https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
 //https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson
+//https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson
 
 //point to the API and queryURL
-var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
 //Get the data from the JSON with D3
 d3.json(quakeURL, function(data) {
