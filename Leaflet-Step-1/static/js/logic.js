46dee87812b2b0aaa367919a08c08e72fa54ed7b
jmgeo <j.mills.geo@gmail.com>
2021-08-09 15:13:37 -0500

    Created the functions/features for earthquakes.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -10,3 +10,24 @@ d3.json(quakeURL, function(data) {
     console.log (data)
     createFeatures(data.features);
 });
+
+//create the functions to run on the features
+function createFeatures(earthquakeData) {
+    var earthquakes = L.geoJSON(earthquakeData), {
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h3> Location: " + feature.properties.place + "</h3><hr><p> Date/Time: " + new Date(feature.properties.time) + "</p><p> Magnitude: " + feature.properties.mag + "</p>"); 
+        },
+
+        pointToLayer: function (feature, latlng) {
+            return new L.circle(latlng,
+              {radius: getRadius(feature.properties.mag),
+              fillColor: circleColor(earthquakeData.geometry.coordinates[2]),
+              fillOpacity: .6,
+              color: "black",
+              stroke: true,
+              weight: .8
+            })
+        }
+    });
+    createMap (earthquakes);
+}
