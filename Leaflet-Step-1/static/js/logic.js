c133f1dd001b8dc736907a7763ea7e6900bf3357
jmgeo <j.mills.geo@gmail.com>
2021-08-09 20:46:58 -0500

    Mostly done with code. I think. Troubleshooting.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -32,7 +32,7 @@ function createFeatures(earthquakeData) {
     createMap (earthquakes);
 }
 
-//create the mapping
+//create the mapping and layers
 function createMap(earthquakes) {
     var topoMap = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}', {
         maxZoom: 20,
@@ -47,9 +47,68 @@ function createMap(earthquakes) {
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3']
     });
+
+    var baseMaps = {
+        "Topo Map": topoMap,
+        "Street Map": streetMap,
+        "Satellite Map": satelliteMap
+    }
 });
 
+// Create an overlay object to hold the overlay layer
+var overlayMaps = {
+    "Earthquakes": earthquakes
+};
+    
+// Create a myMap centered on Houston
+var myMap = L.map("map", {
+    center: [29.75, -95.37],
+    zoom: 3,
+    layers: [topoMap, earthquakes]
+});
+//Add layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false,
+    legend: true
+}).addTo(myMap);
 
+// Create a legend
+var legend = L.control({position: 'bottomleft'});
+    legend.onAdd = function (myMap) {
 
+    var div = L.DomUtil.create('div', 'info legend');
+    labels = [],
+    grades = [0,1,2,3,4,5];
 
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+    };
+    legend.addTo(myMap);
 }
+
+// Create the color function based on the Richter magnitudes
+function getColor(magnitude) {
+    if (magnitude > 7) {
+        return 'red'
+    } else if (magnitude > 6) {
+        return 'orange'
+    } else if (magnitude > 5) {
+        return 'yellow'
+    } else if (magnitude > 4) {
+        return 'lightgreen'
+    } else if (magnitude > 2) {
+        return 'green'
+    } else {
+        return 'blue'
+    }
+};
+
+//Create radius function
+function getRadius(magnitude) {
+    return magnitude * 25000;
+};
+
