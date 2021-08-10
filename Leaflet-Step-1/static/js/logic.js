d473b6616bc2287ce84a0998c80f96c653869c36
jmgeo <j.mills.geo@gmail.com>
2021-08-09 20:10:37 -0500

    Doing the double trouble here working on homework during class.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -43,6 +43,13 @@ function createMap(earthquakes) {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
-    var 
+    var let satelliteMap = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+        maxZoom: 20,
+        subdomains:['mt0','mt1','mt2','mt3']
+    });
 });
+
+
+
+
 }
